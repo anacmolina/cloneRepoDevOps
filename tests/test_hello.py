c16bbc14da90d0144ab844a0465c0186dcd4dfbd
anacmolina <53834507+anacmolina@users.noreply.github.com>
2022-09-08 11:28:04 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,7 +4,7 @@ from desoper import cloneRepoDevOps
 
 class Test_hello(unittest.TestCase):
     def test__working(self):
-        self.assertEqual(hello.hello(),
+        self.assertEqual(cloneRepoDevOps.hello(),
                          'Hello, World!', True)
 
 
